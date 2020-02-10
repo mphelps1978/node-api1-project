035cecf9d38e3d56914e0408cd6b9f57728467aa
mphelps1978 <56881040+mphelps1978@users.noreply.github.com>
2020-02-10 15:22:43 -0600

    GET and POST to /api/users functioning with Validation

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// implement your API here
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.use(express.json())
+const users = require('./data/db')
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+// GET /api/isers - Returns a list of all users
+
+app.get('/api/users', (req, res) => {
+  users.find()
+  .then(users => {
+    res.status(200).json(users)
+  })
+  .catch(err =>{
+  res.status(500).json({errorMessage: 'There was an error'})
+  })
+});
+
+// POST /api/users - Adds a user to the database
+//  - If either name or bio are missing, return 400
+
+app.post('/api/users', (req, res) => {
+  const {name, bio} = req.body
+
+  if (!name || !bio) {
+    res.status(400).json({errorMessage: "Please provide name and bio for user"})
+  } else {
+    users.insert(req.body)
+    .then(user => {
+      res.status(201).json(user)
+    })
+    .catch(err =>{
+      res.status(500).json({errorMessage: 'There was an error'})
+    })
+  }
+
+
+
+});
+
+
+
+
+
+
+app.listen(port, () => console.log(`\n**app listening on port ${port} !`))
